585abce7dd7716f1173ce3fdd2cd6bebed362e1c
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-12-05 13:01:20 +0100

    Update aplay.py

--- aplay.py
+++ aplay.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from subprocess import call
 import time
 
 from time import sleep
@@ -13,18 +14,25 @@ from pprint import pprint
 #from os import walk
 import glob
 
-banki = ['ptaki','zwierzeta','piano1']
 
+#call(["aplay", "v.wav"])
+
+banki = ['ptaki','zwierzeta','piano1']
+ileBankow = len(banki)
+nrBanku = 0;
 def loadBank(bankName):
 	DS='/'
 	bankPath = '/home/pi/voices/'
 	pathBank   = bankPath+bankName+DS
 	return sorted(glob.glob(pathBank+"*.wav"))
 
+   
 
 bank  = loadBank(banki[0])
 #pprint(bank)	
-#stopMPD = subprocess.Popen(["mpc", "stop"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stopMPD = subprocess.Popen(["mpc", "stop"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+setVol = subprocess.Popen(["amixer", "amixer set PCM 70%"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 
 
 print("Sampler Ready.")
@@ -33,7 +41,9 @@ lastBank=8;
 players=[]
 
 for i in range(lastBank):
-	players.append(lambda i: os.system("aplay -q "+bank[i]))
+	players.append(lambda i: call(["aplay","-q", bank[i]]))
+	#players.append(lambda i: os.system("aplay -q "+bank[i]))
+   
 pprint(players)
 '''
 watki=[]
@@ -59,6 +69,16 @@ def playThread7():
 	thread.start_new_thread(players[7], (7,))
 
 
+def preloadBank(nr):
+	global bank
+	global players
+	global lastBank
+	bank = loadBank(banki[nr])
+	for i in range(lastBank):
+		players.append(lambda i: call(["aplay","-q", bank[i]]))
+   
+   
+   
 #time.sleep(2)
 glosy = [playThread0,playThread1,playThread2,playThread3,playThread4,playThread5,playThread6,playThread7]
 
@@ -71,9 +91,12 @@ while True:
 		czas = (random.randint(1,5)*10) / tempo
 		czas += (3*step/tempo)
 		glos = random.choice (glosy)
-		print(step,'czas',czas)	
+		print(nrBanku,step,'czas',czas)	
 		glos()
 		sleep(czas)
+		if (step % 10 == 9):
+			nrBanku = (nrBanku + 1) % ileBankow  
+			preloadBank(nrBanku)
 	except KeyboardInterrupt:
 		exit()
 	  
